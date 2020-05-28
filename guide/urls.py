297c0043fbbea30115685d2e8fd853da5c04ff93
liverms <51294711+liverms@users.noreply.github.com>
2020-05-28 08:41:20 -0400

    Updated urls.py redirect to first form page

--- guide/urls.py
+++ guide/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from guide.views import ajax_type, ajax_code, TradeForm, lt_condition, FORMS, url_name, done_step_name
+from django.shortcuts import redirect
 
 trade_wizard = TradeForm.as_view(
     FORMS, 
@@ -10,9 +11,12 @@ trade_wizard = TradeForm.as_view(
     done_step_name=done_step_name
 )
 
+
 urlpatterns = [
     url(r'^en/(?P<step>.+)/$', trade_wizard, name='form_step'),
     url('/en/done/', trade_wizard, name=done_step_name),
     path("ajax/type/", ajax_type, name = 'ajax_type'),
-	path("ajax/code/", ajax_code, name = 'ajax_code')
+	path("ajax/code/", ajax_code, name = 'ajax_code'),
+    path('', lambda request: redirect(r'en/0/', permanent=False)),
+    url('^.*$', lambda request: redirect(r'en/0/', permanent=False))
 ]
