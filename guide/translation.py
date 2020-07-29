290c3eeb4d5c3f29fa5b40d1f97e9caaff40d6a3
liverms <51294711+liverms@users.noreply.github.com>
2020-07-29 10:25:29 -0400

    added required translation fields

--- guide/translation.py
+++ guide/translation.py
@@ -7,26 +7,32 @@ class OrganizationTO(TranslationOptions):
     fields = ['name']
     required_languages = ('en-ca', 'fr-ca')
 
+
 @register(CommodityType)
 class CommodityTypeTO(TranslationOptions):
     fields = ['commodity_type']
+    required_languages = ('en-ca', 'fr-ca')
 
 
 @register(Code)
 class CodeTO(TranslationOptions):
     fields = ['code']
+    required_languages = ('en-ca', 'fr-ca')
 
 
 @register(LimitedTenderingReason)
 class LimitedTenderingReasonTO(TranslationOptions):
     fields = ['name']
+    required_languages = ('en-ca', 'fr-ca')
 
 
 @register(GeneralException)
 class GeneralExceptionTO(TranslationOptions):
     fields = ['name']
+    required_languages = ('en-ca', 'fr-ca')
 
 
 @register(CftaException)
 class CftaExceptionTO(TranslationOptions):
     fields = ['name']
+    required_languages = ('en-ca', 'fr-ca')
