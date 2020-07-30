d6ff2bfab45f04f6ee565c49d06336410c32b7c5
liverms <51294711+liverms@users.noreply.github.com>
2020-07-30 10:03:07 -0400

    tranlations updated .po file created

--- guide/translation.py
+++ guide/translation.py
@@ -5,34 +5,34 @@ from modeltranslation.translator import register, TranslationOptions
 @register(Organization)
 class OrganizationTO(TranslationOptions):
     fields = ['name']
-    required_languages = ('en-ca', 'fr-ca')
+    required_languages = ('en', 'fr')
 
 
 @register(CommodityType)
 class CommodityTypeTO(TranslationOptions):
     fields = ['commodity_type']
-    required_languages = ('en-ca', 'fr-ca')
+    required_languages = ('en', 'fr')
 
 
 @register(Code)
 class CodeTO(TranslationOptions):
     fields = ['code']
-    required_languages = ('en-ca', 'fr-ca')
+    required_languages = ('en', 'fr')
 
 
 @register(LimitedTenderingReason)
 class LimitedTenderingReasonTO(TranslationOptions):
     fields = ['name']
-    required_languages = ('en-ca', 'fr-ca')
+    required_languages = ('en', 'fr')
 
 
 @register(GeneralException)
 class GeneralExceptionTO(TranslationOptions):
     fields = ['name']
-    required_languages = ('en-ca', 'fr-ca')
+    required_languages = ('en', 'fr')
 
 
 @register(CftaException)
 class CftaExceptionTO(TranslationOptions):
     fields = ['name']
-    required_languages = ('en-ca', 'fr-ca')
+    required_languages = ('en', 'fr')
